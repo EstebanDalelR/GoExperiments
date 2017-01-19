e167ee51482247d6b1808a5bd9fe2841dbae434b
Esteban Dalel R <estebandalelr@gmail.com>
2017-01-19 13:37:37 -0500

    Create 32 Variadic Functions2

---
+++ GoFiles/32VariadicFunctions2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	xs := []int{1, 2, 3}
+	fmt.Println(add(xs...))
+}
+func add(args ...int) int {
+	total := 0
+	for _, v := range args {
+		total += v
+	}
+	return total
+}
