1e23d30ea46efef8c1869125dee41990029a7d37
Esteban Dalel R <estebandalelr@gmail.com>
2017-02-13 23:12:29 -0500

    Create 52 Select

---
+++ GoFiles/52Select.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+"fmt"
+"time"
+)
+
+func main()  {
+  chan1 := make(chan string)
+  chan2 := make(chan string)
+
+  go func ()  {
+    for {
+      chan1 <- "From1"
+      time.Sleep(time.Second * 2)
+    }
+  }()
+  go func ()  {
+    for {
+      chan2 <- "From2"
+      time.Sleep(time.Second * 3)
+    }
+  }()
+  go func ()  {
+    for {
+      select {
+      case msg1 := <- chan1:
+        fmt.Println(msg1)
+      case msg2 := <- chan2:
+        fmt.Println(msg2)
+      }
+    }
+  }()
+
+  var inp string
+  fmt.Scanln(&inp)
+}
